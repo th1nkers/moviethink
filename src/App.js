8f0215a79ddd183eb4050a85952a122178d751dd
thinkers121 <suryasingh11112003@gmail.com>
2023-03-25 23:43:14 +0530

    adding add movie form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, {useState} from 'react';
-
+import React, { useState, useEffect, useCallback } from 'react';
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
 
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
-  async function fetchMoviesHandler(){
-    const response = await fetch('https://swapi.dev/api/films/')
-    const data = await response.json()
-    
-    .then(data => {
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+
+      if (!response.ok && response.status === 404) {
+        throw new Error('Something went wrong!')
+      }
+
+      const data = await response.json()
+
       const transformedMovies = data.results.map(movieData => {
         return {
           id: movieData.episode_id,
@@ -21,16 +30,56 @@ function App() {
         }
       })
       setMovies(transformedMovies)
-    })
+    } catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+  }, [])
+
+  ////////////////////////////////////
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler])
+
+  ////////////////////////////////////
+
+  let content = <p>Found no movies.</p>
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
   }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
+
+  /////////////////////////////////////
+
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
+  /////////////////////////////////////
+
 
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {content}
       </section>
     </React.Fragment>
   );
+}
+
+export default App;
